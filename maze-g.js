d7ea803cca70459806d68ed36cb1d8d3beae044a
juan luiz barraza <99239515+vatoloco13@users.noreply.github.com>
2022-12-16 23:34:17 -0800

    Add files via upload

--- maze-g.js
+++ maze-g.js
@@ -1,57 +1,31 @@
-var myObstacles = [];
-var myGameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
-      this.canvas.width = 1500;
-      this.canvas.height = 700;
-      this.context = this.canvas.getContext("2d");
-      document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-      this.interval = setInterval(updateGameArea, 20);
-    },
-    clear : function() {
-      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    },
-    stop : function() {
-      clearInterval(this.interval);
-    }
+const canvas = document.createElement('canvas');
+const context = canvas.getContext('2d');
+const currentRectX = 425;
+const currentRectY = 3;
+const mazeWidth = 556;
+const mazeHeight =556;
+var intervalVar;
+function drawMazeAndRectangle(rectX, RectY) {
+  makeWhite(0,0, canvas.width, canvas.height);
+  var mazImg = new Image();
+  mazImg.onload = function() {
+    context.drawImage(mazeImg, 0, 0,);
+    drawMazeAndRectangle(rectX, RectY,"#000FF" ,false, true);
+    context.beginPath();
+    context.arc(542, 122, 7, 0, 2* Math.PI, false);
+    context,closePath();
+    context.fillStyle = '#FFFFFF';
+    context.fill();
+  };
+function drawRectangle(x, y, style) {
+  makeWhite(currRectX, currRectY, 15, 15);
+  currentRectX = x;
+  currentRectY = y;
+  context.beginPath();
+  context.rect(x, y, 15, 15);
+  context.closePath();
+  context.fillStyle = style;
+  context.fill();
+}
   }
-  
-  function component(width, height, color, x, y) {
-    this.width = width;
-    this.height = height;
-    this.update = function() {
-      ctx = myGameArea.context;
-      ctx.fillStyle = color;
-      ctx.fillRect(this.width, this.height);
-    }
-    }
-    this.crashWith = function(otherobj) {
-      var myleft = this.x;
-      var myright = this.x + (this.width);
-      var mytop = this.y;
-      var mybottom = this.y + (this.height);
-      var otherleft = otherobj.x;
-      var otherright = otherobj.x + (otherobj.width);
-      var othertop = otherobj.y;
-      var otherbottom = otherobj.y + (otherobj.height);
-      var crash = true;
-      if ((mybottom < othertop) ||
-      (mytop > otherbottom) ||
-      (myright < otherleft) ||
-      (myleft > otherright)) {
-        crash = false;
-      }
-      return crash;
-    }
-  
-  
-  function updateGameArea() {
-    if (myGamePiece.crashWith(myObstacle)) {
-      myGameArea.stop();
-    } else {
-      myGameArea.clear();
-      myObstacle.update();
-      myGamePiece.newPos();
-      myGamePiece.update();
-    }
-  }<script src="index.js"></script>
+
